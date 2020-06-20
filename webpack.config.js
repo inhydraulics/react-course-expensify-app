26ec24cdedf5c7a535fb97a0da21a75d0d382eb1
SmartMonitor <smartindustrialmonitor@gmail.com>
2020-06-20 22:37:51 +0100

    setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,52 @@
 const path = require("path");
-//const webpack = require("webpack");
+//const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-	entry: "./src/app.js",
-	mode: "development",
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /(node_modules|bower_components)/,
-				loader: "babel-loader",
-			},
-			{
-				test: /\.s?css$/,
-				use: ["style-loader", "css-loader", "sass-loader"],
-			},
-		],
-	},
-	resolve: { extensions: ["*", ".js", ".jsx"] },
-	devtool: 'cheap-module-eval-source-map',
-	output: {
-		path: path.resolve(__dirname, "public"),
-		//publicPath: "/dist/",
-		filename: "bundle.js",
-	},
-	devServer: {
-		contentBase: path.join(__dirname, "public"),
-		port: 3000,
-		//publicPath: "http://localhost:3000/dist/",
-		historyApiFallback: true
-	},
+module.exports = (env) => {
+	//console.log('env', env);
+	// const CSSExtract = new ExtractTextPlugin('styles.css');
+	const CSSExtract = new MiniCssExtractPlugin({
+		filename: "styles.css",
+	});
+
+	const isProduction = env === "production";
+	return {
+		entry: "./src/app.js",
+		mode: "development",
+		module: {
+			rules: [
+				{
+					test: /\.(js|jsx)$/,
+					exclude: /(node_modules|bower_components)/,
+					loader: "babel-loader",
+				},
+				{
+					test: /\.s?css$/,
+					//use: ["style-loader", "css-loader", "sass-loader"],
+					// use: CSSExtract.extract({
+					// 	use: ["css-loader", "sass-loader"]
+					// })
+					use: [
+						MiniCssExtractPlugin.loader,
+						{ loader: "css-loader", options: { sourceMap: true } },
+						{ loader: "sass-loader", options: { sourceMap: true } },
+					],
+				},
+			],
+		},
+		plugins: [CSSExtract],
+		resolve: { extensions: ["*", ".js", ".jsx"] },
+		devtool: isProduction ? "source-map" : "inline-source-map",
+		output: {
+			path: path.resolve(__dirname, "public"),
+			//publicPath: "/dist/",
+			filename: "bundle.js",
+		},
+		devServer: {
+			contentBase: path.join(__dirname, "public"),
+			port: 3000,
+			//publicPath: "http://localhost:3000/dist/",
+			historyApiFallback: true,
+		},
+	};
 };
