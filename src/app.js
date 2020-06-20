94d726ae14193fa16ccbf9f73e4dd45d46f26a0e
SmartMonitor <smartindustrialmonitor@gmail.com>
2020-06-20 12:54:38 +0100

    Remove dummy expense data

--- src/app.js
+++ src/app.js
@@ -10,36 +10,7 @@ import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 import "react-dates/lib/css/_datepicker.css";
 
-
 const store = configureStore();
-
-store.subscribe(() => {
-	const state = store.getState();
-});
-
-store.dispatch(
-	addExpense({
-		description: "Water Bill",
-		amount: 100,
-		createdAt: 150,
-	})
-);
-
-store.dispatch(
-	addExpense({
-		description: "Gas Bill",
-		amount: 80,
-		createdAt: 250,
-	})
-);
-
-store.dispatch(
-	addExpense({
-		description: "Rent",
-		amount: 8000,
-		createdAt: 100,
-	})
-);
 
 const jsx = (
 	<Provider store={store}>
